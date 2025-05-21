9d72d25753d032b736b63ad3d3bbb3414e3671a6
Ill <126349149+Ill-27@users.noreply.github.com>
2025-05-21 21:03:45 +0300

    Delete api/token.js

--- api/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken')
-const SECRET = 'ArTERRII_3D-ProT3ct!on_Key_89d2@r0Wz'
-
-module.exports = (req, res) => {
-  const token = jwt.sign({ access: 'ok' }, SECRET, { expiresIn: '5m' })
-
-  res.setHeader('Content-Type', 'application/json')
-  res.status(200).send(JSON.stringify({ token }))
-}
